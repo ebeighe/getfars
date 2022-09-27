02c47ef50f92b2cb3a55f617d269c2d162671c88
ebeighe <ebeighe@msn.com>
2022-09-26 19:40:37 -0700

    Update copy.sh
    don't need $hed since copying all fields

--- src/copy.sh
+++ src/copy.sh
@@ -3,6 +3,9 @@
 # This script takes three arguments: 
 # A zip file containing csvs, a schema name, and a table name, which should match a name of a CSV file in the ZIP
 
+# added for educational purposes ; causes commands to be echoed out
+set -x
+
 zip=$1
 schema=$2
 table=$3
@@ -12,4 +15,6 @@ hed=$(unzip -Cp "$zip" "${table}.csv" | head -n 1 | awk '{sub(/^\xef\xbb\xbf/,""
 
 echo "COPY ${schema}.${table}" 1>&2
 
-unzip -Cp "$zip" "${table}.csv" | psql -c "COPY ${schema}.${table} ( $hed ) FROM STDIN (FORMAT csv, HEADER true, ENCODING 'windows-1252')"
+# as things turn out, since COPYing all the fields, the whole $hed thing is unnecesary
+# unzip -Cp "$zip" "${table}.csv" | psql -c "COPY ${schema}.${table} ( $hed ) FROM STDIN (FORMAT csv, HEADER true, ENCODING 'windows-1252')"
+unzip -Cp "$zip" "${table}.csv" | psql -c "COPY ${schema}.${table} FROM STDIN (FORMAT csv, HEADER true, ENCODING 'windows-1252')"
